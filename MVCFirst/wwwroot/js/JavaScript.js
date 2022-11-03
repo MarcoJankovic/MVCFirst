4f41ac995779057096ecd4e1d0f3bcbdb5231317
Marco Jankovic <Marcojankovice@hotmail.com>
2022-11-03 15:34:06 +0100

    New Exe

--- MVCFirst/wwwroot/js/JavaScript.js
+++ MVCFirst/wwwroot/js/JavaScript.js
@@ -2,7 +2,7 @@
     context = canvas.getContext('2d');
 context.globalCompositeOperation = 'lighter';
 canvas.width = 500;
-canvas.height = 1200;
+canvas.height = 800;
 canvas.index = 10;
 draw();
 
