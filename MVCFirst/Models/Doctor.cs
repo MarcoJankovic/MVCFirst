ffaa68d4b1eb91562aff9b617d9ccb8abd2824fc
Marco Jankovic <Marcojankovice@hotmail.com>
2022-11-03 09:18:54 +0100

    new

--- MVCFirst/Models/Doctor.cs
+++ MVCFirst/Models/Doctor.cs
@@ -4,15 +4,11 @@
     {
         public static string Diagnose(double temp)
         {
-            bool temperature = false;
-            while (!temperature)
-            {
                 if (temp == 37)
                 {
                     return "You have perfectly well, go home!";
-                    temperature = true;
                 }
-                else if (temp > 36 && temp < 38)
+                else if (temp >= 36 && temp <= 38)
                 {
                     return "You should get some rest!";
                 }
@@ -28,10 +24,8 @@
                 {
                     return "Rest in Peace ";
                 }
-
-            }
+      
             return "Temp outside";
-            //return (temp > 37) ? "Almost healty...have a cookie" : "No fever..Do not take a cookie!";
 
         }
     }
