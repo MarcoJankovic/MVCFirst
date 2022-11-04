1baa97b1ab6ad83eef2c9ae3ce391cb82d0c83f4
Marco Jankovic <Marcojankovice@hotmail.com>
2022-11-04 08:57:42 +0100

    nwew

--- MVCFirst/Controllers/GuessController.cs
+++ MVCFirst/Controllers/GuessController.cs
@@ -4,6 +4,22 @@ namespace MVCFirst.Controllers
 {
     public class GuessController : Controller
     {
+        [HttpPost]
+        public IActionResult Random(int number, int random)
+        {
+            if (number != random)
+            {
+                ViewBag.Msg = Models.Guess.deRandom();
+                return View();
+            }
+            else
+            {
+                ViewBag.Msg = "Enter a number please!";
+                return View();
+            }
+        }
+
+        [HttpGet]
         public IActionResult Random()
         {
             return View();
---
+++ MVCFirst/Models/Guess.cs
@@ -0,0 +1,17 @@
+﻿namespace MVCFirst.Models
+{
+    public class Guess
+    {
+        public static string deRandom()
+        {
+            Random random = new Random();
+
+            for(int j = 0; j <4; j++)
+            {
+                var rnd = random.Next(1, 101);     
+            }
+            
+            return "";
+        }
+    }
+}
