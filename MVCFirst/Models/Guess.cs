68a25640344f30fab4f901bce375ee02dfd7f506
Marco Jankovic <Marcojankovice@hotmail.com>
2022-11-06 20:15:21 +0100

    newss

--- MVCFirst/Models/Guess.cs
+++ MVCFirst/Models/Guess.cs
@@ -9,12 +9,5 @@
             Random random = new Random();
             Number = random.Next(1, 101);
         }
-        public void RandomNum(int guess)
-
-        {
-            Random random = new Random();
-            Number = random.Next(1, 101);
-        }
-
     }
 }
